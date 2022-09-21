324d78860fd8a1e0d73b4e37495102f1d7475764
Jorge Ossa <jorgehossa@gmail.com>
2022-09-20 23:31:54 -0500

    style: 🎨 Add card styles & refactor handleChange

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../styles/Button.css";
 import "../styles/Modal.css";
+import "../styles/Card.css";
 import background from "../assets/card-bg.png";
 
 function Button({
@@ -37,6 +38,12 @@ function Modal({ ctaAction, onChange }) {
           placeholder="Nombre"
           onChange={onChange}
         />
+        <input
+          className="modal__input"
+          name="lastName"
+          placeholder="Apellido"
+          onChange={onChange}
+        />
         <Button type="button" onClick={ctaAction}>
           Continuar
         </Button>
@@ -47,13 +54,19 @@ function Modal({ ctaAction, onChange }) {
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const [userName, setUserName] = useState("");
+  const [guest, setGuest] = useState({
+    firstName: "",
+    lastName: "",
+  });
   const handleClick = (e) => {
     e.preventDefault();
     setIsModalOpen(!isModalOpen);
   };
   const handleInputChange = (e) => {
-    setUserName(e.target.value);
+    setGuest((guest) => ({
+      ...guest,
+      [e.target.name]: e.target.value,
+    }));
   };
   return (
     <section className="app" style={{ backgroundImage: `url(${background})` }}>
@@ -61,27 +74,29 @@ function App() {
         <Modal ctaAction={handleClick} onChange={handleInputChange} />
       )}
       {!isModalOpen && (
-        <section>
-          <h1>
-            <span>{`¡${userName} te esperamos`}</span>
-            <span>Cumpleaños</span>
-            <span>de</span>
-            <span>DaniSaurio</span>
-          </h1>
-          <p>2 de Octubre - 3pm</p>
-          <p>
-            EnCantoJardín Musical <br />
-            Cra. 33 #21-39
-          </p>
-          <Button url="https://www.google.com" target="_blank">
-            Confirmar
-          </Button>
-          <p>
-            <span role="img" aria-label="star">
-              ⭐
-            </span>{" "}
-            Niños y niñas de jean y camiseta blanca
-          </p>
+        <section className="card">
+          <div className="card__wrapper">
+            <p className="card__intro">{`¡${guest.firstName} te esperamos!`}</p>
+            <h1 className="card__title">
+              <span className="title__top">Cumpleaños</span>
+              <span className="title__center">de</span>
+              <span className="title__bottom">DaniSaurio</span>
+            </h1>
+            <p className="card__date">2 de Octubre - 3pm</p>
+            <p className="card__address">
+              EnCantoJardín Musical <br />
+              Cra. 33 #21-39
+            </p>
+            <div className="card__button">
+              <Button>Confirmar</Button>
+            </div>
+            <p className="card__footer">
+              <span role="img" aria-label="star">
+                ⭐
+              </span>{" "}
+              Niños y niñas de jean y camiseta blanca
+            </p>
+          </div>
         </section>
       )}
     </section>
